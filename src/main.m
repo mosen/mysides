d942380e1c4d76105bf61e537d006a4b50d41b12
Mosen <mosen1@gmail.com>
2014-11-10 09:49:22 +1100

    Remove commented code from main

--- src/main.m
+++ src/main.m
@@ -23,7 +23,7 @@ void print_help(char const *arg0)
     NSLog(@"Usage:\n");
     NSLog(@"\t%s list\t- list sidebar items", arg0);
     NSLog(@"\t%s add <name> <uri> [after]\t- append a sidebar item to the end of the list, or after the given name\n", arg0);
-    NSLog(@"\t%s insert <name> <uri> [before]\t- insert a sidebar item at the start of the list, or before the given name\n", arg0);
+    //NSLog(@"\t%s insert <name> <uri> [before]\t- insert a sidebar item at the start of the list, or before the given name\n", arg0);
     NSLog(@"\t%s remove <name>\t- remove a sidebar item\n", arg0);
 }
 
@@ -133,10 +133,6 @@ void sidebar_list()
 
 int main (int argc, char const *argv[])
 {
-    
-
-//    CFStringRef desiredNameRef = NULL;
-    
     if(argc >= 2) {
         if (strcmp(argv[1], "list") == 0) {
             sidebar_list();
@@ -167,63 +163,6 @@ int main (int argc, char const *argv[])
         print_help(argv[0]);
         return 1;
     }
-    
-//    if(argc >= 2) {
-//        desiredNameRef = CFStringCreateWithCStringNoCopy(kCFAllocatorDefault, argv[1], kCFStringEncodingUTF8, NULL);
-//        if(argc >= 3) {
-//            type = atol(argv[2]);
-//            if(!type && strlen(argv[2]) != 4) {
-//                NSLog(@"type must be 4 characters");
-//                return 254;
-//            }
-//            if(!type) type = (argv[2][3] << 0) + (argv[2][2] << 8) + (argv[2][1] << 16) + (argv[2][0] << 24);
-//        }
-//    }
-    
 
-    
-   
-    
-//    LSSharedFileListItemRef sflItemBeforeRef = (LSSharedFileListItemRef)kLSSharedFileListItemBeforeFirst;
-    
-//    for(NSObject *object in list) {
-//        LSSharedFileListItemRef sflItemRef = (__bridge LSSharedFileListItemRef)object;
-//        CFStringRef nameRef = LSSharedFileListItemCopyDisplayName(sflItemRef);
-//        CFURLRef urlRef = NULL;
-//        LSSharedFileListItemResolve(sflItemRef, kLSSharedFileListNoUserInteraction | kLSSharedFileListDoNotMountVolumes, &urlRef, NULL);
-//        
-//        if(!desiredNameRef) {
-//            UInt32 itemId = LSSharedFileListItemGetID(sflItemRef);
-//            
-//            NSLog(@"[%u] %@ -> %@", itemId, (__bridge id)nameRef, (__bridge id)urlRef);
-//            if(urlRef)  CFRelease(urlRef);
-//            if(nameRef) CFRelease(nameRef);
-//            
-//            CFStringRef props[] = {
-//                // kLSSharedFileListItemClass,
-//                kLSSharedFileListItemTemplateSystemSelector,
-//                kLSSharedFileListSpecialItemIdentifier,
-//                kLSSharedFileListItemManaged,
-//            };
-//            int i;
-//            for(i = 0; i < sizeof(props)/sizeof(*props); i++) {
-//                CFTypeRef propRef = LSSharedFileListItemCopyProperty(sflItemRef, props[i]);
-//                NSLog(@" %p: %@ = %@ (%@)", props[i], (__bridge id)props[i], (__bridge id)propRef, propRef ? (id)CFBridgingRelease(CFCopyTypeIDDescription(CFGetTypeID(propRef))) : nil);
-//                if(propRef) CFRelease(propRef);
-//            }
-//        } else {
-//            if(CFEqual(nameRef, desiredNameRef)) {
-//                if(!type) {
-//                    NSLog(@"Cleared: %@", nameRef);
-//                    LSSharedFileListInsertItemURL(sflRef, sflItemBeforeRef, nameRef, NULL, urlRef, NULL, (__bridge CFArrayRef)[NSArray arrayWithObject:(__bridge id)kLSSharedFileListItemTemplateSystemSelector]);
-//                } else {
-//                    LSSharedFileListItemSetProperty(sflItemRef, kLSSharedFileListItemTemplateSystemSelector, CFNumberCreate(kCFAllocatorDefault, kCFNumberSInt32Type, &type));
-//                }
-//            }
-//        }
-//        sflItemBeforeRef = sflItemRef;
-//    }
-//    
-//    CFRelease(sflRef);
     return 0;
 }
