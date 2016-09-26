ef33449e7dd8538a35ebf481b06d2e0747154dee
Mosen <mosen1@gmail.com>
2016-09-26 10:56:26 +1000

    Fixed return status of sidebar_add() FIX: #9

---
+++ src/SharedFileList.h
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+
+@interface SharedFileList : NSObject
+
+@end
---
+++ src/SharedFileList.m
@@ -0,0 +1,13 @@
+//
+//  Sidebar.m
+//  mysides
+//
+//  Created by Eamon Brosnan on 14/04/2016.
+//  Copyright © 2016 com.github.mosen. All rights reserved.
+//
+
+#import "SharedFileList.h"
+
+@implementation SharedFileList
+
+@end
